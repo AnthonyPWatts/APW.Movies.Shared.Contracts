8ce00361339bb891b8d4747b34ec909ea0139204
Anthony Watts <anthonypwatts@gmail.com>
2024-04-28 13:35:09 +0100

    Added method signature for getting a single MovieActor by id of Movie and Actor

--- src/AnthonyPWatts.Movies.Shared.Contracts/Interfaces/IMovieActorRepository.cs
+++ src/AnthonyPWatts.Movies.Shared.Contracts/Interfaces/IMovieActorRepository.cs
@@ -5,6 +5,7 @@ namespace AnthonyPWatts.Movies.Shared.Contracts.Interfaces;
 public interface IMovieActorRepository
 {
     public Task<IEnumerable<MovieActorDto>> GetAllAsync();
+    public Task<MovieActorDto?> GetByIdAsync(int movieId, int actorId);
     public Task<IEnumerable<MovieActorDto>> GetAllByMovieIdAsync(int id);
     public Task<IEnumerable<MovieActorDto>> GetAllByActorIdAsync(int id);
     public Task<MovieActorDto?> AddAsync(MovieActorDto movieActor);
